c6e537f888b946d7b31a4719a45a52c8a09aa9b3
Burbruee <Burbruee@603c31ae-2744-11df-8e46-3928208ddfa0>
2010-03-04 18:59:08 +0000

    Very basic Sprite Class Screen resolution set to 1280x720 Textures loading (player, enemy) Drawing sprites to screen.

---
+++ trunk/TROLLArena/Classes/Sprite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TROLLArena
+{
+    class Sprite
+    {        
+        public Vector2 position;
+        public float rotation;
+        public float scale;
+        public Texture2D texture;
+        public Vector2 origin;
+
+        public Sprite(Vector2 position)
+        {
+            this.position = position;
+            this.scale = 1f;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D texture, Vector2 position, Color color, float rotation, Vector2 origin, float scale, SpriteEffects spriteEffects)
+        {            
+            spriteBatch.Draw(texture, position, null, color, rotation, origin, scale, spriteEffects, 0f);
+        }
+    }
+}
