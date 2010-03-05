e0b9b59bf3840872892cc12c94ecef2f36314e0c
Burbruee <Burbruee@603c31ae-2744-11df-8e46-3928208ddfa0>
2010-03-05 03:20:14 +0000

    Background added.

--- TROLLArena/Game1.cs
+++ TROLLArena/Game1.cs
@@ -19,10 +19,13 @@ namespace TROLLArena
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;                
+        SpriteBatch spriteBatch;
 
         Sprite player;
         Sprite enemy;
+        Sprite bg;
+
+        protected Vector2 mousePos;
 
         public Game1()
         {
@@ -39,9 +42,9 @@ namespace TROLLArena
         /// </summary>
         protected override void Initialize()
         {
-            graphics.PreferredBackBufferWidth = 1024;
-            graphics.PreferredBackBufferHeight = 600;
-            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.IsFullScreen = false;
             
             graphics.ApplyChanges();
             base.Initialize();            
@@ -55,26 +58,26 @@ namespace TROLLArena
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            
             player = new Sprite(Vector2.Zero);
             enemy = new Sprite(Vector2.Zero);
-            
-            player.texture = Content.Load<Texture2D>(@"Textures\TROLLET_HD_small");
-            enemy.texture = Content.Load<Texture2D>(@"Textures\cat");          
+            bg = new Sprite(Vector2.Zero);
 
-            player.scale = 1f;
-            
+            player.texture = Content.Load<Texture2D>(@"Textures\TROLLET_HD_small");
+            enemy.texture = Content.Load<Texture2D>(@"Textures\cat");
+            bg.texture = Content.Load<Texture2D>(@"Textures\background");          
+
+            player.scale = 1f;            
             player.position = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight - player.texture.Height / 2);            
             player.origin = new Vector2(player.texture.Width / 2, player.texture.Height / 2);
 
             Random random = new Random();
             
-            enemy.scale = 1f;
-            //enemy.position = new Vector2(graphics.PreferredBackBufferWidth / 2, enemy.texture.Height);
+            enemy.scale = 1f;            
             enemy.position = new Vector2(random.Next(enemy.texture.Width / 2, graphics.PreferredBackBufferWidth - enemy.texture.Width / 2), random.Next(enemy.texture.Height / 2, graphics.PreferredBackBufferHeight - enemy.texture.Height / 2));
             enemy.origin = new Vector2(enemy.texture.Width / 2, enemy.texture.Height / 2);
             enemy.vX = random.Next(-8, 8);
-            enemy.vY = random.Next(-8, 8);
-
+            enemy.vY = random.Next(-8, 8);            
         }
 
         /// <summary>
@@ -90,16 +93,27 @@ namespace TROLLArena
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>        
+
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
+            MouseState mouseState = Mouse.GetState();
+
+            mousePos = new Vector2(mouseState.X, mouseState.Y);
+            
             enemy.rotation = (float)gameTime.TotalGameTime.TotalSeconds * (float)1.5;
             enemy.position.X += enemy.vX;
             enemy.position.Y += enemy.vY;
+          
+            if (mousePos.X < player.origin.X) mousePos.X = player.origin.X;
+            if (mousePos.Y < player.origin.Y) mousePos.Y = player.origin.Y;
+            if (mousePos.X > graphics.PreferredBackBufferWidth - player.origin.X) mousePos.X = graphics.PreferredBackBufferWidth - player.origin.X;
+            if (mousePos.Y > graphics.PreferredBackBufferHeight - player.origin.Y) mousePos.Y = graphics.PreferredBackBufferHeight - player.origin.Y;
+
 
             if (enemy.position.X < enemy.texture.Width / 2) enemy.vX = -enemy.vX;
             if (enemy.position.X > graphics.PreferredBackBufferWidth - enemy.texture.Width / 2) enemy.vX = -enemy.vX;
@@ -116,19 +130,12 @@ namespace TROLLArena
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            MouseState mouseState = Mouse.GetState();
-            Vector2 mousePos = new Vector2(mouseState.X, mouseState.Y);
-
+            
             spriteBatch.Begin(SpriteBlendMode.AlphaBlend);
 
-            if (mousePos.X < player.origin.X) mousePos.X = player.origin.X;
-            if (mousePos.Y < player.origin.Y) mousePos.Y = player.origin.Y;
-            if (mousePos.X > graphics.PreferredBackBufferWidth - player.origin.X) mousePos.X = graphics.PreferredBackBufferWidth - player.origin.X;
-            if (mousePos.Y > graphics.PreferredBackBufferHeight - player.origin.Y) mousePos.Y = graphics.PreferredBackBufferHeight - player.origin.Y;
-
+            bg.Draw(spriteBatch, bg.texture, bg.position, new Color(255, 255, 255, 255), bg.rotation, bg.origin, bg.scale, SpriteEffects.None);
             player.Draw(spriteBatch, player.texture, mousePos, Color.White, player.rotation, player.origin, player.scale, SpriteEffects.None);
-
-            enemy.Draw(spriteBatch, enemy.texture, enemy.position, new Color(255, 255, 255, 255), enemy.rotation, enemy.origin, enemy.scale, SpriteEffects.None);
+            enemy.Draw(spriteBatch, enemy.texture, enemy.position, new Color(255, 255, 255, 255), enemy.rotation, enemy.origin, enemy.scale, SpriteEffects.None);            
             
             spriteBatch.End();
 
