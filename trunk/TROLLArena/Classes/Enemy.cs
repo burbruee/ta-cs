07156710fab5da518d0578df15da0ccce92662e5
burbruee <burbruee@603c31ae-2744-11df-8e46-3928208ddfa0>
2010-03-06 00:41:04 +0000

    Sprite class redone. Collisions need work.

---
+++ trunk/TROLLArena/Classes/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TROLLArena
+{
+    class Enemy : Actor
+    {
+        private float speed;
+        private Actor target;
+
+        public Enemy(Texture2D texture, Actor target, float speed) : base(texture)
+        {
+            this.target = target;
+            this.speed = speed;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (target == null)
+                return;
+
+            Vector2 direction = Vector2.Normalize(target.Position - this.position);
+            this.position += direction * speed;
+        }
+    }
+}
