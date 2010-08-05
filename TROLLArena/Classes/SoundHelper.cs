d581714d824f8567031759c4464c307ae082b3bf
burbruee <burbruee@603c31ae-2744-11df-8e46-3928208ddfa0>
2010-08-05 13:31:07 +0000

    Added new class: SoundHelper

---
+++ TROLLArena/Classes/SoundHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework.Audio;
+
+namespace TROLLArena
+{
+    static class SoundHelper
+    {
+        static AudioEngine audioEngine;
+        static WaveBank waveBank;
+        static SoundBank soundBank;        
+        
+        static string[] sounds;
+
+        public static void Initialize()
+        {
+            audioEngine = new AudioEngine(@"Content\Sounds\Audio.xgs");
+                        
+            waveBank = new WaveBank(audioEngine, @"Content\Sounds\Wave Bank.xwb");
+            soundBank = new SoundBank(audioEngine, @"Content\Sounds\Sound Bank.xsb");
+
+            sounds = new string[] { "jallabertmrkruuuuk", "bonk", "gameover" };
+        }
+
+        public static void Update()
+        {
+            audioEngine.Update();
+        }
+
+        public static void PlaySound(int index)
+        {
+            soundBank.PlayCue(sounds[index]);            
+        }
+
+        public static void StopSound(int index)
+        {
+            soundBank.Dispose();
+        }
+    }
+}
